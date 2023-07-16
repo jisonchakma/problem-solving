af656ad1b03e19ebb6a178282bb077a3c60a8358
Jison Chakma <jisonchakma2332@gmail.com>
2023-07-16 23:55:31 +0600

    Problem Solving Week_6 Day3

---
+++ Week_6/Day3/print_bianry_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        cout<<1<<" ";
+        else
+        cout<<0<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
