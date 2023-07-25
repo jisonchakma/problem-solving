681facfa706889527209c24fd4f719f4e3df3dcb
Jison Chakma <jisonchakma2332@gmail.com>
2023-07-25 22:24:56 +0600

    Problem Solving Week_7 Day5

---
+++ Week_7/Day5/just_more_one_episode.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int x;
+    cin>>x;
+    if(x>24)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
