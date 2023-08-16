e4321039f3aea66bf61b0ece9ced51b28855e031
Jison Chakma <jisonchakma2332@gmail.com>
2023-08-16 23:50:00 +0600

    Problem Solving Week_10 Day6

---
+++ Week_10/Day6/oly_string.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll ans=0;
+    vector<ll>ans1,ans2;
+    for(ll j=0;j<n;j++)
+    {
+        ll m;
+        cin>>m;
+        vector<ll>a(m);
+        for(ll i=0;i<m;i++)
+        cin>>a[i];
+        sort(a.begin(),a.end());
+        ans1.push_back(a[0]);
+        ans2.push_back(a[1]);
+ 
+    }
+    sort(ans1.begin(),ans1.end());
+    sort(ans2.begin(),ans2.end());
+    reverse(ans2.begin(),ans2.end());
+    ans2.pop_back();
+    ans+=ans1[0];
+    for(auto val:ans2)
+    ans+=val;
+    cout<<ans<<endl;
+    
+}
+int main()
+{
+    int t;
+    cin>>t;
+    int cs=1;
+    for(int i=cs;i<=t;i++)
+    {
+    solve();
+    }
+    return 0;
+}
---
+++ Week_10/Day6/sort_the_subarray.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll>a(n);
+    vector<ll>b(n);
+    for(ll i=0;i<n;i++)
+    cin>>a[i];
+    for(ll i=0;i<n;i++)
+    cin>>b[i];
+    ll l=0,r=n-1;
+    for(ll i=0;i<n;i++)
+    {
+        if(a[i]==b[i])
+        continue;
+        l=i;
+        break;
+    }
+    for(ll i=n-1;i>=0;i--)
+    {
+        if(a[i]==b[i])
+        continue;
+        r=i;
+        break;
+    }
+    while(l-1>0&&b[l-1]<=b[l])
+    l--;
+    while(r+1<n&&b[r+1]>=b[r])
+    r++;
+    vector<pair<ll,ll>>v;
+    for(ll i=l;i<r;i++)
+    {
+         if(b[i+1]<b[i])
+         {
+         v.push_back({l,i});
+         l=i+1;
+         }
+    }
+    v.push_back({l,r});
+    l=0,r=0;
+    ll mx=0;
+    for(auto i:v)
+    {
+        if(i.second-i.first+1>mx)
+        {
+            mx=i.second-i.first+1;
+            l=i.first;
+            r=i.second;
+        }
+
+    }
+    cout<<l+1<<" "<<r+1<<"\n";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    solve();
+    return 0;
+}
